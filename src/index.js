e370e72c63925773b03f3093af2390134c31e285
mistrynirali <127983716+mistrynirali@users.noreply.github.com>
2024-03-06 18:24:08 +0530

    fixed

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+
+import connectDb from "./db/index.js";
+
+dotenv.config({path: "./env"})
+
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+
+})
+.catch((err) => {
+    console.log("Mongodb connection fail", err)
+})
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+            app.on("errr", (error) => {
+            console.com("ERROR: ", error)
+            throw error
+            })
+
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listing on port ${process.env.PORT}`)
+            })
+
+    }catch(error){
+        console.com("ERROR: ", error)
+        throw error
+    }
+})()
+*/
